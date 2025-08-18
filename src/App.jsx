3be3d83c8c0575f460feee4370a12b43040e2a6c
momo4646 <mariamiyah1@gmail.com>
2025-08-18 01:09:50 -0700

    use state to re render each time new ingredient is added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from '/src/components/Header'
 import Main from '/src/components/Main'
 
 export default function App() {
+
     return(
         <>
             <Header />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,30 @@
+import React from 'react'
 
 export default function Main() {
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-    const ingredientsList = ingredients.map(ingredient => <li>{ingredient}</li>)
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsList = ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)
     
     function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
-        const newIngredient = formData.get('ingredient')
-        ingredients.push(newIngredient)
-        console.log(ingredientsList)
-    }
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        setIngredients(prev => [...prev, newIngredient])
+    };
+
     return(
         <main>
             <form className='add-ingredient-form' onSubmit={handleSubmit}>
                 <input 
                     name='ingredient'
                     aria-label='Add ingredient' 
-                    placeholder='eg. oregano'/>
-                <button >Add Ingredient</button>        
+                    placeholder='eg. oregano'
+                    type='text' />
+                <button type='submit'>Add Ingredient</button>        
             </form>
             <ul>
                 {ingredientsList}
             </ul>
         </main>
-    )
-}
+    );
+};
